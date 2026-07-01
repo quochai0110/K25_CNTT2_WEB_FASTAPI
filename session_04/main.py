233c48b355b872b301f46c0444772f716bbd36ce
C4EJS100-TrinhQuocHai <trinhquochai12@gmail.com>
2026-07-01 11:02:47 +0700

    add

---
+++ session_04/main.py
@@ -0,0 +1,90 @@
+﻿"""
+1. path parameter :
+    đường dẫn có tham số
+    
+2. query parameter :
+    truy vấn có tham số 
+3. request body
+4. type hints
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# lấy dữ liệu trong db
+students = [
+    {"id": 1, "name": "Phan Đức Anh", "age": 20},
+    {"id": 2, "name": "Lê Minh Đức", "age": 21},
+    {"id": 3, "name": "Vũ Hồng Vân", "age": 20},
+]
+app = FastAPI()
+
+class Student(BaseModel):
+    name:str
+    age: int
+    id : int
+    
+@app.get("/students")
+def get_students():
+    return {"message": "lấy danh sách sinh viên thành công!",
+            "data": students}
+# lấy chi tiết thông tin của 1 bạn sinh viên
+
+@app.get("/students/{student_id}")
+def get_student_detail(student_id):
+    #  trả về thông tin chi tiết của sinh viên
+    print("giá trị id nhận về", student_id)
+    for student in students:
+        if str(student["id"]) == student_id:
+            return {
+                "message": "lấy chi tiết sinh viên thành công?",
+                "data": student
+            }
+    return{
+            "message":"không tìm thấy sinh viên",
+            "data":[]
+        }
+
+# query parameter : tìm kiếm, lọc, phân trang
+# lọc các sinh viên có tuổi 21
+
+@app.get("/search")
+# def search_student_by_age(age):
+#     print("giá trị tuổi nhận vào", age)
+#     result = []
+#     for student in students:
+#         if str(student["age"]) == age:
+#             result.append(student) 
+#     return{
+#         "số lượng sinh viên tìm lấy":len(result),
+#         "data": result
+#     }
+def search_student_by_name(keyword,age):
+    # print("keyword: ", keyword)
+    # print("age:",age)
+    result = []
+    for student in students:
+        if keyword.lower() in student["name"].lower() and int(age) == student["age"]:
+            result.append(student)
+    return {
+        "data":result
+    }
+#  query nhiều điền kiện thì làm thế nào? &
+
+# request body
+# thêm sinh viên vào lớp 
+@app.post("/students")
+def create_student(student:Student):
+    new_student = {
+        "id":student.id,
+        "name":student.name,
+        "age":student.age
+    }
+    print("thông tin sinh viên cần thêm", new_student)
+    # print("đã gọi phương thức để thêm sinh viên",student)
+    return{
+        "message":"thêm sinh viên thành công!"
+    }
+
+
+    
---
+++ session_05/main.py
@@ -0,0 +1,85 @@
+﻿from fastapi import FastAPI
+from pydantic import BaseModel
+
+"""
+CRUD:
+C: CREATE
+R: READ
+U: UPDATE
+D: DELETE
+
+"""
+
+""" QUẢN LÝ SẢN PHẨM  """
+# TẠO DỮ LIỆU DANH SÁCH SẢN PHẨM BAN ĐẦU.
+products = [
+    {"id": 1, "product_name": "iphone 14", "price": 15000000, "stock": 15},
+    {"id": 2, "product_name": "iphone 15", "price": 19000000, "stock": 3},
+    {"id": 3, "product_name": "iphone 16", "price": 25000000, "stock": 8},
+]
+app = FastAPI()
+
+
+#  Viết API hiển thị danh sách tất cả sản phẩm
+@app.get("/products")
+def get_products():
+    return {"message": "lấy danh sách tất cả sản phẩm", "data": products}
+
+
+# Viết API lấy chi tiết 1 sản phẩm
+@app.get("/products/{id}")
+def get_product_display(id: int):
+    for product in products:
+        if product["id"] == id:
+            return {"message": "hiển tị thông tin chi tiết sản phẩm", "data": product}
+    return {"message": "sản phẩm không tồn tại", "data": ""}
+
+
+class Product(BaseModel):
+    id: int
+    product_name: str
+    price: int
+    stock: int
+# Viết API thêm sản phẩm
+@app.post("/products")
+def add_product(product: Product):
+    new_product = {
+        "id": products[-1]["id"] + 1,
+        "product_name": product.product_name,
+        "price": product.price,
+        "stock": product.stock,
+    }
+    print("sản phẩm vừa mới thêm", new_product)
+    products.append(new_product)
+    return {"message": "thêm sản phẩm thành công!", "data": products}
+
+
+#  Viết API xóa sản phẩm
+
+
+@app.delete("/products/{product_id}")
+def delete_product(product_id):
+    print("id sản phẩm cần xóa", product_id)
+    for index, product in enumerate(products):
+        if int(product_id) == product["id"]:
+            result = products.pop(index)
+            return {"message": "xóa sản phẩm thành công", "data": result}
+    return {"message": "xóa sản phẩm thất bại", "data": ""}
+# Viết API cập nhật sản phẩm
+# 2 method PUT||PATCH
+@app.put("/products/{product_id}")
+def update_product(product_id, product:Product):
+    print("id sản phẩm cần cập nhật",product_id)
+    print("thông tin giá trị cần cập nhật", product)
+    for i in products:
+        if i["id"] == product_id:
+            i["product_name"] = product.product_name
+            i["price"] = product.price
+            i["stock"] = product.stock
+            return {
+                "message":"cập nhật thành công ",
+                "data":product
+            }
+    return  {
+        "message":"cập nhật thất bại"
+    }
